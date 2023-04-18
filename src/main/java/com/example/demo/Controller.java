5c1106f45bade5dcfe87c66c9e98b4c765e291b3
Aegidius <Aegidius.Haslauer@Student.HTW-Berlin.de>
2023-04-18 12:41:42 +0200

    tried out @RequestMapping

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     
     @GetMapping("/")
-	public String index(){ return "Mo stinkt"; }
+	public String index(){ return "Hello World!"; }
+    
+    @RequestMapping(path="/test/")
+    public String queryParameter(@RequestParam("name") String name){
+        return name;
+    }
+
 
 }
