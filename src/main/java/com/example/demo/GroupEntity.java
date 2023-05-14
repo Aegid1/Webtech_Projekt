6a01cf66ce3a469dc29ed3794a08a13657e8ff6d
Aegidius <Aegidius.Haslauer@Student.HTW-Berlin.de>
2023-05-14 11:26:29 +0200

    just a small name change

---
+++ src/main/java/com/example/demo/GroupEntity.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class GroupEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private List<UserEntity> members;    
+
+    
+}
